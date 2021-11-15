b18f8693d34a8bddb7622e16980e2067914fd945
Chiara Campalani <campalanichiara@gmail.com>
2021-11-15 16:34:45 +0900

    Created seed file & related gems

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # To handle db cleanup
+  gem 'database_cleaner-active_record'
+  # To create seeds
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'database_cleaner/active_record'
+require 'faker'
+
+puts "Doing some cleaning🧹. . ."
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
+puts "Ready for new seeds🌱"
+
+puts "Creating Items. ."
+
+item_1 = Item.create({ name: "KitKat - Peach Parfait Flavour", brand: "Nestle", barcode: Faker::Barcode.ean(13)})
+puts "Generated item 1: #{item_1.name} with barcode #{item_1.barcode}!"
+
+item_2 = Item.create({ name: "KitKat - Mango Flavour", brand: "Nestle", barcode: Faker::Barcode.ean(13)})
+puts "Generated item 2: #{item_2.name} with barcode #{item_2.barcode}"
+
+item_3 = Item.create({ name: "Pocky Coconut", brand: "Glico", barcode: Faker::Barcode.ean(13)})
+puts "Generated item 3: #{item_3.name} with barcode #{item_3.barcode}"
+
+puts "All items generated!"
+
+
+puts "Generating items amounts.."
+
+items_array = [item_1, item_2, item_3]
+10.times do
+  ItemAmount.create!({
+    amount: rand(50),
+    exp_date: Faker::Date.between(from: 30.days.from_now, to: 1.year.from_now),
+    item: items_array[rand(3)]
+  })
+end
+
+puts "All items amoounts generated!"
+puts "Seed completed - DB ready to use"
+
+
