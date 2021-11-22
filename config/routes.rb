ccfe00678898b0ccc014a2658d2b74417a183e60
Chiara Campalani <campalanichiara@gmail.com>
2021-11-22 19:52:22 +0900

    Fixed routing error & added navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items
   root to: 'items#home'
-  get 'items', to: 'items#index'
-  post 'items', to: 'items#create'
-  get 'items/new', to: 'items#new'
-  get 'items/:id/edit', to: 'items#edit'
-  get 'item/:id', to: 'items#show', as: :item
-  patch 'items/:id', to: 'items#update'
-  delete 'items/:id', to: 'items#destroy'
 end
