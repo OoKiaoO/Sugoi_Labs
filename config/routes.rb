1eb5cf18d5eab4e54c8beec5c7aefbd02a61b781
Chiara Campalani <campalanichiara@gmail.com>
2021-12-19 19:35:57 +0900

    created devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items do
     resources :item_amounts, only: [ :new, :create ]
