f1867db3973619b975aef8c5da5bc9e730b87679
Chiara Campalani <campalanichiara@gmail.com>
2022-06-05 16:26:52 +0900

    created migrations to include additional details to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,13 @@
 class Item < ApplicationRecord
   has_many :item_amounts, dependent: :destroy
 
+  CATEGORIES = ['snacks', 'candies', 'chips', 'chocolates', 'soft drinks', 'seasonings', 'sauces', 'noodles']
+
   validates :name, presence: true
   validates :brand, presence: true
-  validates :barcode, presence: true, numericality: true, length: { is: 13 }
+  validates :barcode, presence: true, numericality: true, length: { is: 13 }, uniqueness: true
+  validates :barcode, numericality: true, length: { is: 13 }, uniqueness: true
+  validates :category, presence: true, :inclusion => { :in => CATEGORIES }
 
   include PgSearch::Model
   pg_search_scope :search_by_all_item_info,
