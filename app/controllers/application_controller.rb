af9681edc977d5d37e7629fc0199c2a4240cdea6
Chiara Campalani <campalanichiara@gmail.com>
2022-06-27 07:17:35 +0900

    added logic to log item_amounts deletion in logs on item show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def log(item, item_amount=nil, exp_date=nil, action)
+  def log(item, action, item_amount=nil, exp_date=nil)
     @log = ActivityLog.new
     @log.user = current_user
     @log.item_id = item
